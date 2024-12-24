74190fe7224f3321c2319a0a01e57ddf0ccca864
YeMaolin <2351925363@qq.com>
2024-12-24 16:45:00 +0800

    移动鼠标

--- README.md
+++ README.md
@@ -55,3 +55,50 @@ cmake ..
 make
 ./LaserPointerMouse.exe
 ```
+
+指定生成器
+
+```shell
+mkdir build
+cd build
+cmake .. -G "MinGW Makefiles"
+make
+./LaserPointerMouse.exe
+```
+
+## 移动鼠标
+
+屏幕坐标原点在左上角
+
+```cpp
+#include <windows.h>
+#include <iostream>
+#include <sstream>
+void moveMouse(int x, int y) {
+    SetCursorPos(x, y);
+    std::cout << "mouse's position" << std::endl;
+    std::cout << "x: " << x << " y: " << y << std::endl;
+}
+int main() {
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);   // 获取屏幕宽度
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);  // 获取屏幕高度
+    std::cout << "Screen Width: " << screenWidth << std::endl;
+    std::cout << "Screen Height: " << screenHeight << std::endl;
+    std::string input;
+    while (true) {
+        std::getline(std::cin, input);  // 获取以回车为结束的字符串
+        if (input == "q" || input == "Q") {
+            break;
+        }
+        std::istringstream iss(input);
+        int x, y;
+        if (iss >> x >> y) {
+            moveMouse(x, y);
+        }
+    }
+    return 0;
+}
+
+```
+
+#
--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
+#include <windows.h>
 #include <iostream>
-#include <opencv2/opencv.hpp>
-using namespace cv;
-using namespace std;
-inline void log(string message) {
-    cout << message << endl;
+#include <sstream>
+void moveMouse(int x, int y) {
+    SetCursorPos(x, y);
+    std::cout << "mouse's position" << std::endl;
+    std::cout << "x: " << x << " y: " << y << std::endl;
 }
-int main(void) {
-    log("start...");
-    cv::Mat img = cv::imread("C:/Users/Yezi/Desktop/picture/background/BKR.png");
-    cv::imshow("maolinye", img);
-    cv::waitKey();
-    log("over...");
+int main() {
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);   // 获取屏幕宽度
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);  // 获取屏幕高度
+    std::cout << "Screen Width: " << screenWidth << std::endl;
+    std::cout << "Screen Height: " << screenHeight << std::endl;
+    std::string input;
+    while (true) {
+        std::getline(std::cin, input);  // 获取以回车为结束的字符串
+        if (input == "q" || input == "Q") {
+            break;
+        }
+        std::istringstream iss(input);
+        int x, y;
+        if (iss >> x >> y) {
+            moveMouse(x, y);
+        }
+    }
     return 0;
 }
